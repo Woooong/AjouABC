e1cf61f2d9a0d87e3a36715eab8d50fc62973430
Ryunhee Han <ryuneeee@gmail.com>
2018-04-30 20:17:08 +0900

    Adding Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from models import db, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 
 @app.route("/")
@@ -8,5 +10,20 @@ def hello():
     return "Hello World!"
 
 
+@app.route("/user/new")
+def new_user():
+    u = User(username='ryun', password='12345678')
+    db.session.add(u)
+    db.session.commit()
+    return "OK"
+
+
+def init_database():
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
+
 if __name__ == '__main__':
-    app.run()
+    init_database()
+    app.run(port=5000)
---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from passlib.handlers import bcrypt
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = bcrypt.encrypt(password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
