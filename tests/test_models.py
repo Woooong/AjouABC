3b4343bae14e4c90f980c85a6b2799f3738adeac
Ryunhee Han <ryuneeee@gmail.com>
2018-05-10 19:53:59 +0900

    Add Emotion

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,9 @@
-import bcrypt
+import json
 
-from models import User, SALT
+import bcrypt
+from datetime import date
+
+from models import User, SALT, Emotion
 
 
 def test_user():
@@ -9,3 +12,51 @@ def test_user():
     u = User(username=name, password=passwd)
     assert u.username == name
     assert u.password == bcrypt.hashpw(passwd.encode(), SALT)
+
+
+def test_create_emotion():
+    str = '''{
+            "anger": 0.037,
+            "contempt": 0.001,
+            "disgust": 0.015,
+            "fear": 0.001,
+            "happiness": 0.939,
+            "neutral": 0.001,
+            "sadness": 0.0,
+            "surprise": 0.007
+          }'''
+
+    u = User(username='ryun', password='aloha')
+    e = Emotion(user=u, str=str, date=date.today())
+
+    parsed = json.loads(str)
+    assert parsed['anger'] == e.anger
+    assert parsed['contempt'] == e.contempt
+    assert parsed['disgust'] == e.disgust
+    assert parsed['fear'] == e.fear
+    assert parsed['happiness'] == e.happiness
+    assert parsed['neutral'] == e.neutral
+    assert parsed['sadness'] == e.sadness
+    assert parsed['surprise'] == e.surprise
+
+    assert date.today() == e.date
+
+
+def test_user_create_emotion():
+
+    u = User(username='ryun', password='aloha')
+    u.id = 1
+    str = '''{
+        "anger": 0.037,
+        "contempt": 0.001,
+        "disgust": 0.015,
+        "fear": 0.001,
+        "happiness": 0.939,
+        "neutral": 0.001,
+        "sadness": 0.0,
+        "surprise": 0.007
+      }'''
+
+    e = Emotion(user=u, str=str, date=date.today())
+    u.append_emotion(e)
+    assert u.emotions[0] == e
